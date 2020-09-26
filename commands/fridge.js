7e7e67b4e76bcf02077f6c0380f9c179af8693dc
Fabio <fabio.farina92@gmail.com>
2020-09-27 09:33:11 +1000

    Random fridge links

--- commands/fridge.js
+++ commands/fridge.js
@@ -3,6 +3,7 @@ module.exports = {
 	description: 'ERP Fridge',
 	enabled: true,
 	execute(config, message, args) {
-		message.channel.send('https://www.youtube.com/watch?v=TiC8pig6PGE&vl=en');
+		let fridgeLinks = ['https://youtu.be/LuLtt_bSByw?t=26', 'https://www.youtube.com/watch?v=TiC8pig6PGE&vl=en' ]
+		message.channel.send(fridgeLinks[Math.floor(Math.random() * fridgeLinks.length)])
 	},
 };
